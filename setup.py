2f936b128dac86c14c001452393ea146b0bee6d1
Fakhar <fakharamirali@gmail.com>
2023-08-30 12:35:15 +0430

    Increase Version #3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VCA',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     url='https://github.com/fakharamirali/version-control-application.git',
     license='MIT',
